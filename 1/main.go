df84aaec2fa781a2d420b908b12ff30e44a06163
James Abley <james.abley@gmail.com>
2018-12-03 22:38:54 +0000

    1 part 1

---
+++ 1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	var currentFrequency int64
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		change, err := strconv.ParseInt(line, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		currentFrequency += change
+	}
+
+	fmt.Println(currentFrequency)
+}
